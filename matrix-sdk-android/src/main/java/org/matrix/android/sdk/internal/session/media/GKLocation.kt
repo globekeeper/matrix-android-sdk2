ab6a92cabe597bbcd0ae6b511263f4f96586728e
alex <oleksii.perets@globekeeper.com>
2022-09-03 00:12:25 +0200

    time added to GKLocation

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/media/GKLocation.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/media/GKLocation.kt
@@ -8,6 +8,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 @JsonClass(generateAdapter = true)
 data class GKLocation(val type: String,
+                      val time: Long,
                       @Json(name = "geometry") val geometry: Geometry,
                       @Json(name = "properties") val properties: Properties) : Parcelable
 
