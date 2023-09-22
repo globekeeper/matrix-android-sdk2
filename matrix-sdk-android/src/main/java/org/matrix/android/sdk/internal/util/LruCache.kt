ed11186b5d834fd7bb4fce107b6fc827aca0ed36
alex <oleksii.perets@globekeeper.com>
2023-09-22 12:09:54 +0200

    Revert "gradle, kotlin and jdk updated"
    This reverts commit a2d545214f99d32b42fd4be29737c98bcd128edf.

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/util/LruCache.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/util/LruCache.kt
@@ -18,6 +18,7 @@ package org.matrix.android.sdk.internal.util
 
 import androidx.collection.LruCache
 
-internal inline fun <K, V> LruCache<K, V>.getOrPut(key: K & Any, defaultValue: () -> V & Any): V {
+@Suppress("NULLABLE_TYPE_PARAMETER_AGAINST_NOT_NULL_TYPE_PARAMETER")
+internal inline fun <K, V> LruCache<K, V>.getOrPut(key: K, defaultValue: () -> V): V {
     return get(key) ?: defaultValue().also { put(key, it) }
 }
